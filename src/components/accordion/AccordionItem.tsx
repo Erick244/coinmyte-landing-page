2d223ee9e6bcc7ce7e5f53450f5593c548a6591a
Erick244 <erienrique03@gmail.com>
2024-04-01 16:52:05 -0300

    fix a visual glitch in accordion

--- src/components/accordion/AccordionItem.tsx
+++ src/components/accordion/AccordionItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { ChevronDown } from "lucide-react";
+import { ChevronUp } from "lucide-react";
 import { HTMLAttributes } from "react";
 import { H1 } from "../typography/H1";
 
@@ -23,11 +23,11 @@ export function AccordionItem({
                 {...props}
                 className={cn(
                     "transition-all duration-300 border-2 bg-gradient-to-r to-transparent to-100% from-transparent from-40% border-white w-full flex justify-between items-center p-3 rounded",
-                    isActive ? "rounded-b-none to-primary" : "rounded-b"
+                    isActive ? "to-primary" : "rounded-b"
                 )}
             >
                 <H1 className="text-base sm:text-base">{title}</H1>
-                <ChevronDown
+                <ChevronUp
                     className={cn(
                         "transition-all duration-300",
                         isActive ? "rotate-0" : "rotate-180"
@@ -36,8 +36,8 @@ export function AccordionItem({
             </button>
             <p
                 className={cn(
-                    "text-sm overflow-y-scroll transition-all duration-300 border-2 border-white border-t-0 rounded-b p-5",
-                    isActive ? "py-5 h-36" : "py-0 h-0 border-b-0"
+                    " text-sm overflow-y-scroll transition-all duration-300 border border-white border-t-0 rounded-b p-5",
+                    isActive ? "py-5 h-36" : "py-0 h-0 border-0 rounded-none"
                 )}
             >
                 {children}
